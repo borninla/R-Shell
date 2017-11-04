dcfd877618b7fcd04dcf3bcbe2fbd3623ae91e50
Ryan Villena <rvill028@hammer.cs.ucr.edu>
2017-11-03 22:20:44 -0700

    Added documentation

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int main()
 {
     Manager m;
+    cout << "Hello world!!!!!!!!!!!!!!!!" << endl;
     m.run();
 
     return 0;
--- manager.h
+++ manager.h
@@ -8,8 +8,6 @@
 #include <cstring>
 #include <cstdlib>
 #include <wait.h>
-#include <wait.h>
-#include <wait.h>
 
 using namespace std;
 
@@ -19,7 +17,14 @@ public:
     void run();
 private:
     void execute(char **command);
+
+    /**
+ * @brief Populates command (an array of cstrings) with cstrings delimited by whitespace
+ * @param line Beginning of cstring containing all args
+ * @param command array of cstrings which this function will populate
+ */
     void parse(char *line, char **command);
+    void populateQueue();
 
     bool wasSuccess;
     queue<string> tokens;
